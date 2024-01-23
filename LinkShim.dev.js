b4f0542981922ca460e7ae1dd495a92135131813
meet bhanushali <lcs2021023@iiitl.ac.in>
2024-01-24 02:13:24 +0530

    updated

--- LinkShim.dev.js
+++ LinkShim.dev.js
@@ -1,27 +1,32 @@
 var LinkShim = {
-
-    //set options
-    redirect_url: "http://yoururl.com:8888/r?",
+    // Configuration options
+    redirect_url: "http://localhost:8888/r?",
     tracker_attr: "data-track",
     params: {},
 
-    //called on mousedown event
     changeHref: function(a) {
         var $a = $(a);
         var params = this.params;
-        var url = this.redirect_url + $a.attr(this.tracker_attr);
+        var url = this.redirect_url;
 
-        //build params                    
+        // Add any tracking attributes to the URL
+        var trackData = $a.attr(this.tracker_attr);
+        if (trackData) {
+            url += trackData + "&";
+        }
+
+        // Build params                    
         params.href = $a.attr('href');
         for (var f in params) {
-            url += "&" + f + "=" + encodeURIComponent(params[f]);
+            url += f + "=" + encodeURIComponent(params[f]) + "&";
         }
-    
-        //change href of click            
+
+        // Trim the trailing '&' and set the new URL
+        url = url.slice(0, -1);
         $a.attr('href', url);
     },
 
-    //manually call to add PageParams
+    // Manually call to add PageParams
     addPageParams: function(new_params) {
         if (typeof new_params == 'object') {
             for (var f in new_params) {
@@ -30,12 +35,13 @@ var LinkShim = {
         }
     },
 
-    //call to listen to clicks on objects that contain the tracker_attr par            
+    // Call to listen to clicks on objects that contain the tracker_attr
     init: function() {
-        var obj = this; //added b/c 'this' will end up refering to a tag
-        $("a[" + obj.tracker_attr + "]").bind('mousedown', function(e) {
+        var obj = this;
+        $("a[" + obj.tracker_attr + "]").on('click', function(e) {
             e.preventDefault();
             obj.changeHref(this);
+            window.location = $(this).attr('href');
         });
     }
 };
